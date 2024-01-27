ec34212abb0943b17519878997980f5a1fa0ee2f
Syamsul Huda Harisul Muslimin <syamsulhuda748@gmail.com>
2024-01-27 19:08:19 +0700

    mengubah source code dari menggunakan grid.js menjadi menggunakan bootstrap

--- module2.js
+++ module2.js
@@ -1,33 +1,43 @@
 const hello = () => {
-    const table = document.getElementById("table");
+    const app = document.getElementById("app");
+    const header = ["Name", "Email", "Phone Number"];
+    const table = document.createElement('table');
+    table.classList.add('table');
 
-    const grid = new gridjs.Grid({
-        columns: ["Name", "Email", "Phone Number"],
-        data: [
-            ["John", "john@example.com", "(353) 01 222 3333"],
-            ["Mark", "mark@gmail.com", "(01) 22 888 4444"],
-            ["Eoin", "eoin@gmail.com", "0097 22 654 00033"],
-            ["Sarah", "sarahcdd@gmail.com", "+322 876 1233"],
-            ["Afshin", "afshin@mail.com", "(353) 22 87 8356"]
-        ],
-        search: {
-            enabled: true
-        },
-        sort: true,
-        pagination: {
-            limit: 3
-        },
-        style: {
-            td: {
-                border: '1px solid #ccc'
-            },
-            table: {
-                'font-size': '20px'
-            }
-        }
-    })
+    const thead = document.createElement('thead');
+    const headerRow = document.createElement('tr');
+    header.forEach(headerText => {
+        const th = document.createElement('th');
+        th.textContent = headerText;
+        headerRow.appendChild(th);
+    });
 
-    grid.render(table);
-  };
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
 
-  export {hello};
+    const tbody = document.createElement('tbody');
+
+    const data = [
+        ["John", "john@example.com", "(353) 01 222 3333"],
+        ["Mark", "mark@gmail.com", "(01) 22 888 4444"],
+        ["Eoin", "eoin@gmail.com", "0097 22 654 00033"],
+        ["Sarah", "sarahcdd@gmail.com", "+322 876 1233"],
+        ["Afshin", "afshin@mail.com", "(353) 22 87 8356"]
+    ];
+
+    data.forEach(rowData => {
+        const row = document.createElement('tr');
+        rowData.forEach(cellData => {
+            const cell = document.createElement('td');
+            cell.textContent = cellData;
+            row.appendChild(cell);
+        });
+        tbody.appendChild(row);
+    });
+
+    table.appendChild(tbody);
+    app.appendChild(table);
+
+};
+
+export { hello };
